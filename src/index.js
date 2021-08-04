04222c1b848ef14c13dcba0d49ef97dd89e16927
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-04 18:24:57 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style.css";
 
-const form = document.querySelector("form");
 const email = document.getElementById("email");
 const country = document.getElementById("country");
 const zipcode = document.getElementById("zip-code");
@@ -17,58 +16,86 @@ function showValidStatus(invalid, valid) {
 	valid.style.visibility = "visible";
 }
 
-email.addEventListener("blur", () => {
+email.addEventListener("input", () => {
 	const invalid = document.querySelector("#email-container .status-invalid");
 	const valid = document.querySelector("#email-container .status-valid");
 
 	if (email.validity.typeMismatch || email.validity.valueMissing) {
 		showErrorStatus(invalid, valid);
+		email.setCustomValidity("Please put your email in here.");
 	} else {
 		showValidStatus(invalid, valid);
+		email.setCustomValidity("");
 	}
 });
 
-country.addEventListener("blur", () => {
+country.addEventListener("input", () => {
 	const invalid = document.querySelector("#country-container .status-invalid");
 	const valid = document.querySelector("#country-container .status-valid");
 
-	if (country.validity.tooLong || country.validity.valueMissing) {
+	if (country.validity.patternMismatch) {
 		showErrorStatus(invalid, valid);
+		country.setCustomValidity("Please put a valid country in here.");
+	} else if (country.validity.valueMissing) {
+		showErrorStatus(invalid, valid);
+		country.setCustomValidity("Don't leave this empty.");
 	} else {
 		showValidStatus(invalid, valid);
+		country.setCustomValidity("");
 	}
 });
 
-// Check minlength
-zipcode.addEventListener("blur", () => {
+zipcode.addEventListener("input", () => {
 	const invalid = document.querySelector("#zip-code-container .status-invalid");
 	const valid = document.querySelector("#zip-code-container .status-valid");
 
-	if (zipcode.validity.tooLong || zipcode.validity.valueMissing) {
+	if (zipcode.validity.patternMismatch) {
 		showErrorStatus(invalid, valid);
+		zipcode.setCustomValidity(
+			"Only letters and numbers is allowed, at least 4 characters long."
+		);
+	} else if (zipcode.validity.valueMissing) {
+		showErrorStatus(invalid, valid);
+		zipcode.setCustomValidity("Don't leave this empty.");
 	} else {
 		showValidStatus(invalid, valid);
+		zipcode.setCustomValidity("");
 	}
 });
 
-// Check minlength
-password.addEventListener("blur", () => {
+password.addEventListener("input", () => {
 	const invalid = document.querySelector("#password-container .status-invalid");
 	const valid = document.querySelector("#password-container .status-valid");
 
-	if (password.validity.valueMissing) {
+	if (password.validity.patternMismatch) {
 		showErrorStatus(invalid, valid);
+		password.setCustomValidity(
+			"Password need to be at least 8 characters long."
+		);
+	} else if (password.validity.valueMissing) {
+		showErrorStatus(invalid, valid);
+		password.setCustomValidity("You need to create your password.");
 	} else {
 		showValidStatus(invalid, valid);
+		password.setCustomValidity("");
 	}
 });
 
-// Check to the same value as password
-confirmPassword.addEventListener("blur", () => {
+confirmPassword.addEventListener("input", () => {
 	const invalid = document.querySelector(
 		"#confirm-password-container .status-invalid"
 	);
 	const valid = document.querySelector(
 		"#confirm-password-container .status-valid"
 	);
+
+	if (confirmPassword.value !== password.value) {
+		showErrorStatus(invalid, valid);
+		confirmPassword.setCustomValidity(
+			"This need to be the same as password above."
+		);
+	} else {
+		showValidStatus(invalid, valid);
+		confirmPassword.setCustomValidity("");
+	}
 });
